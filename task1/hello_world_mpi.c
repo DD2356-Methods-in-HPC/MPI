3ed351d4fe25d408c088aaf22086ddca8c5fbd3b
Elliot <elliot.darth@gmail.com>
2024-04-22 22:35:41 +0200

    Task1 code, not tested yet

---
+++ task1/hello_world_mpi.c
@@ -0,0 +1,23 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int processes, rank;
+
+    // initialize MPI environment
+    MPI_Init(NULL, NULL);
+
+    // get the number of processes
+    MPI_Comm_size(MPI_COMM_WORLD, &processes);
+
+    // get the rank ID of the current process
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // print the hello world message for each MPI process
+    printf("Hello World from rank %d from %d processes!\n", rank, processes);
+
+    // finalize the MPI environment
+    MPI_Finalize();
+
+    return 0;
+}
