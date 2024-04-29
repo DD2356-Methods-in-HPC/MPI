0eaccce343b4efec311addb7510a8625490a9093
Gustaf Larsson <gustaf.larsson01@gmail.com>
2024-04-29 21:51:45 +0200

    script updated

--- task4/runner.sh
+++ task4/runner.sh
@@ -4,19 +4,19 @@
 #SBATCH -t 1:00:00
 #SBATCH -A edu24.DD2356
 #SBATCH -p main
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=256
 #SBATCH --cpus-per-task=2
 #SBATCH --nodes=1
 #SBATCH -e error_file.e
 
 srun -n 8 ./a.out > output
 
-srun -n 16 ./a.out > output
+srun -n 16 ./a.out >> output
 
-srun -n 32 ./a.out > output
+srun -n 32 ./a.out >> output
 
-srun -n 64 ./a.out > output
+srun -n 64 ./a.out >> output
 
-srun -n 128 ./a.out > output
+srun -n 128 ./a.out >> output
 
-srun -n 256 ./a.out > output
+srun -n 256 ./a.out >> output
