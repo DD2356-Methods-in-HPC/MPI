d0e108fe0f86b1566c6d4957cbbe0de24f777621
Elliot <elliot.darth@gmail.com>
2024-05-01 01:34:18 +0200

    hard test

--- bonus/matrix_multiply.py
+++ bonus/matrix_multiply.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # File path
-file_path = 'test_medium.txt'
+file_path = 'test_hard.txt'
 
 # Read input dimensions and matrices from the file
 with open(file_path, 'r') as file:
