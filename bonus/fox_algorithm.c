c3c6753ab2419a84d311c4925898d87e1fa75789
Elliot <elliot.darth@gmail.com>
2024-05-07 00:21:25 +0200

    Update fox_algorithm.c

--- bonus/fox_algorithm.c
+++ bonus/fox_algorithm.c
@@ -322,10 +322,10 @@ int main(int argc, char** argv) {
     for (int step = 0; step < p; step++) {
         printf("Fox algorithm running on process %d, step %d:\n", rank, step);
 
-        /*
         // calculate root process for this step
         int root = (grid_coords[0] + step) % p;
 
+        /*
         // broadcast the block A in each row
         if (grid_coords[1] == root) {
             MPI_Bcast(local_A, TILE_SIZE * TILE_SIZE, MPI_DOUBLE, 0, row_comm);
