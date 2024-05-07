dd35971266f5ad42d51301d193d622d96dea8c25
Elliot <elliot.darth@gmail.com>
2024-05-07 02:18:56 +0200

    Update fox_algorithm.c

--- bonus/fox_algorithm.c
+++ bonus/fox_algorithm.c
@@ -340,7 +340,7 @@ int main(int argc, char** argv) {
         // calculate root process for this step
         // alt: (rank % p + step) % p
         // else rank => grid_coords[0]
-        int root = (grid_coords[1] + step) % p;
+        int root = (rank + step) % p;
 
         //printf("Root: %d, Grid Coordinates: %d, %d", root, grid_coords[0], grid_coords[1]);
 
