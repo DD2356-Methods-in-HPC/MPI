d3b0dc10d4e39d91529ebd0bdfaa0782f287b8ef
Elliot <elliot.darth@gmail.com>
2024-05-12 15:19:12 +0200

    add code for polyfit

--- bonus/data/visualize.py
+++ bonus/data/visualize.py
@@ -1,12 +1,20 @@
 # source venv/scripts/activate
+# python \data\\visualize.py
 
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
+# boolean to tell if we should print polyfit line or not
+PLOT_POLYFIT = True
+# change to which data file should be read
+MATRIX_DATA = 144
+
+# get correct path
 current_directory = os.getcwd()
 
 # read data from file
-file_name = "data_144.txt"
+file_name = f"data_{MATRIX_DATA}.txt"
 file_path = os.path.join(current_directory, "data", file_name)
 
 # get the values we are intressted in (n-task and runtimes)
@@ -52,4 +60,32 @@ plt.plot(x, y, marker='o', linestyle='-')
 plt.xlabel('Number of Processes')
 plt.ylabel('Average Runtime (seconds)')
 plt.grid(True)
+#plt.show()
+
+# polyfit
+def calculate_plolyfit(x, y, degree=1):
+    """Calculates function coefficients from the data using linear regression (polyfit)."""
+    
+    # fit a polynomial of the specified degree to the data using polyfit
+    p = np.polyfit(x, y, degree)
+    
+    return p
+
+polynomial_degree = 2   # if degree is 1, then cut off should be 2
+if polynomial_degree == 1:
+    cut_off = 2             # 2 because we skip 16 and 36
+else:
+    cut_off = 0
+
+coefficients = calculate_plolyfit(x[cut_off:], y[cut_off:], polynomial_degree)
+# print coefficients
+print("coefficients:", coefficients)
+
+# plot the fitted line
+model_x = np.array(x[cut_off:])
+model_y = np.polyval(coefficients, model_x)
+if PLOT_POLYFIT == True:
+    plt.plot(model_x, model_y, color='red', label='Fitted Line')
+
+plt.legend(loc='upper right')
 plt.show()
